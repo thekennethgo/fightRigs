263787664ac30c50cb9af8a05cda1b813fb9f77b
kennethgo <gennethko@gmail.com>
2025-05-19 20:53:25 +1000

    Installed Flask-CORS and tested api calling (From server to front end)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:8080/api/bro')
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <>
